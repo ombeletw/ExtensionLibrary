ef83b6a81b024f1a708e79a2e755d81b23158497
Nicolas Pierre <nico_kestens@hotmail.com>
2014-11-19 10:45:14 +0100

    Added TypeLess IEnumerableExtenisons

---
+++ ExtensionLibrary/List/EnumerableExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionLibrary.List
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Gives an typeless IEnumerable the possibilty to check if it contains items
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool Any(this IEnumerable source)
+        {
+            return Count(source) != 0;
+        }
+
+        /// <summary>
+        /// Gives an typeless IEnumerable the possibilty to return it's count
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static int Count(IEnumerable source)
+        {
+            int res = 0;
+
+            if (source != null)
+            {
+                foreach (var item in source)
+                    res++;
+            }
+
+            return res;
+        }
+    }
+}
