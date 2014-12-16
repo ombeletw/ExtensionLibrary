4629c247d3faf7d7d2ab91ac16f0a5e813d454ec
Nicolas Pierre <nico_kestens@hotmail.com>
2014-12-16 11:22:48 +0100

    Commit transpose

--- ExtensionLibrary/List/EnumerableExtensions.cs
+++ ExtensionLibrary/List/EnumerableExtensions.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace ExtensionLibrary.List
@@ -34,6 +37,69 @@ namespace ExtensionLibrary.List
             }
 
             return res;
+        }
+
+        private static readonly MethodInfo GetValueMethod =
+       (from m in typeof(PropertyInfo).GetMethods()
+        where m.Name == "GetValue" && !m.IsAbstract
+        select m).First();
+
+        private static readonly ConstantExpression NullObjectArrayExpression =
+            Expression.Constant(null, typeof(object[]));
+
+        public static IEnumerable Transpose<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            return TransposeCore(source);
+        }
+
+        private static Delegate CreateSelectorFunc<T>(IEnumerable<T> source)
+        {
+            T[] list = source.ToArray();
+            DynamicProperty[] dynamicProperties =
+                list.Select(i => new DynamicProperty(i.ToString(), typeof(object))).ToArray();
+
+            Type transposedType = ClassFactory.Instance.GetDynamicClass(dynamicProperties);
+
+            ParameterExpression propParam = Expression.Parameter(typeof(PropertyInfo), "prop");
+
+            var bindings = new MemberBinding[list.Length];
+            for (int i = 0; i < list.Length; i++)
+            {
+                MethodCallExpression getter =
+                    Expression.Call(
+                        propParam,
+                        GetValueMethod,
+                        Expression.Constant(list[i]),
+                        NullObjectArrayExpression
+                        );
+
+                bindings[i] = Expression.Bind(transposedType.GetProperty(dynamicProperties[i].Name), getter);
+            }
+
+            LambdaExpression selector =
+                Expression.Lambda(
+                    Expression.MemberInit(
+                        Expression.New(transposedType),
+                        bindings),
+                    propParam);
+
+            return selector.Compile();
+        }
+
+        private static IEnumerable TransposeCore<T>(IEnumerable<T> source)
+        {
+            List<PropertyInfo> properties = typeof(T).GetProperties().ToList();
+            Delegate selector = CreateSelectorFunc(source);
+
+            foreach (PropertyInfo property in properties)
+            {
+                yield return selector.DynamicInvoke(property);
+            }
         }
     }
 }
