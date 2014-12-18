ae780244c32d2034cf8386e48d52a0bfbbe93233
Wim Ombelets <wim.ombelets@gmail.com>
2014-12-18 09:59:09 +0100

    changed namespace for EnumerableExtensions from List to Enumerable to avoid confusion (List is an implementation of IEnumerable, not the other way around)

--- ExtensionLibrary/List/EnumerableExtensions.cs
+++ ExtensionLibrary/List/EnumerableExtensions.cs
@@ -7,7 +7,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
-namespace ExtensionLibrary.List
+namespace ExtensionLibrary.Enumerable
 {
     public static class EnumerableExtensions
     {
