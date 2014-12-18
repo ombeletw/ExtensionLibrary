accd04c0596a9a47cca4561a96cd20bdeb720d28
Wim Ombelets <wim.ombelets@gmail.com>
2014-12-18 09:54:41 +0100

    Added IEnumerable<T>.IsNullOrEmpty

--- ExtensionLibrary/List/EnumerableExtensions.cs
+++ ExtensionLibrary/List/EnumerableExtensions.cs
@@ -38,5 +38,16 @@ namespace ExtensionLibrary.List
 
             return res;
         }
+
+        /// <summary>
+        /// Checks if generic IEnumerable is null or empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || enumerable.Any();
+        }
     }
 }
