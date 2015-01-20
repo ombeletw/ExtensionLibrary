b7a2819c67dcf258aaba0a4b954bea5896ccc997
Wim Ombelets <wim.ombelets@gmail.com>
2015-01-20 09:42:52 +0100

    Added WebControlExtensions.TryResetSelectedIndex

---
+++ ExtensionLibrary/WebControls/WebControlsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace System.Web.UI.WebControls
+{
+    public static class WebControlsExtensions
+    {
+        private static bool TryResetSelectedIndex(this DropDownList ddl)
+        {
+            if (ddl.Items != null && ddl.Items.Count > 0)
+            {
+                ddl.SelectedIndex = 0;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
