da069c88013d567d4d9a9f0c4d7057fd3435b38d
Nicolas Pierre <nico_kestens@hotmail.com>
2015-03-30 15:39:21 +0200

    Added FindVisualChild Method

--- ExtensionLibrary/General/GeneralExtensions.cs
+++ ExtensionLibrary/General/GeneralExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Windows;
+using System.Windows.Media;
 
 namespace ExtensionLibrary.General
 {
@@ -18,6 +17,23 @@ namespace ExtensionLibrary.General
         public static bool Between<T>(this T actual, T lower, T upper) where T : IComparable<T>
         {
             return actual.CompareTo(lower) >= 0 && actual.CompareTo(upper) < 0;
+        }
+
+        public static childItem FindVisualChild<childItem>(DependencyObject obj) where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
         }
     }
 }
