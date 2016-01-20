dc47a47967f8fe8b780bb0b06d2368ce27a38c3d
nicolaspierre1990 <nico_kestens@hotmail.com>
2016-01-20 15:03:59 +0100

    Added InheritsFrom and GetParentTypes

--- ExtensionLibrary/TypeExtension/TypeExtensions.cs
+++ ExtensionLibrary/TypeExtension/TypeExtensions.cs
@@ -19,5 +19,74 @@ namespace ExtensionLibrary.TypeExtension
 
             return type.IsGenericType && type.GetGenericTypeDefinition().Equals(typeof(Nullable<>));
         }
+
+        /// <summary>
+        /// Get if the given baseType is baseClass of given type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="baseType"></param>
+        /// <returns></returns>
+        public static bool InheritsFrom(this Type type, Type baseType)
+        {
+            // null does not have base type
+            if (type == null)
+            {
+                return false;
+            }
+
+            // only interface can have null base type
+            if (baseType == null)
+            {
+                return type.IsInterface;
+            }
+
+            // check implemented interfaces
+            if (baseType.IsInterface)
+            {
+                return type.GetInterfaces().Contains(baseType);
+            }
+
+            // check all base types
+            var currentType = type;
+            while (currentType != null)
+            {
+                if (currentType.BaseType == baseType)
+                {
+                    return true;
+                }
+
+                currentType = currentType.BaseType;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get all baseTypes of given Type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IEnumerable<Type> GetParentTypes(this Type type)
+        {
+            // is there any base type?
+            if ((type == null) || (type.BaseType == null))
+            {
+                yield break;
+            }
+
+            // return all implemented or inherited interfaces
+            foreach (var i in type.GetInterfaces())
+            {
+                yield return i;
+            }
+
+            // return all inherited types
+            var currentBaseType = type.BaseType;
+            while (currentBaseType != null)
+            {
+                yield return currentBaseType;
+                currentBaseType = currentBaseType.BaseType;
+            }
+        }
     }
 }
